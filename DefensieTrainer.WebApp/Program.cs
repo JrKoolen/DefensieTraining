bfee10a315805197d73031839cf026f3d691c4ce
JrKoolen <81234655+JrKoolen@users.noreply.github.com>
2024-05-21 20:13:30 +0200

    update

--- DefensieTrainer.WebApp/Program.cs
+++ DefensieTrainer.WebApp/Program.cs
@@ -44,7 +44,7 @@ app.MapControllerRoute(
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Account}/{action=Login}/{id?}");
 
 
 
